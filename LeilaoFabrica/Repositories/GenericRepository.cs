e07032053759d7429606e76a2526ff27e65997cb
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-25 22:49:59 -0200

    Interface IGenericRepository

---
+++ LeilaoFabrica/Repositories/GenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeilaoFabrica.Repositories
+{
+    public class GenericRepository<T> : IGenericRespoitory<T> where T : class 
+    {
+    }
+}
