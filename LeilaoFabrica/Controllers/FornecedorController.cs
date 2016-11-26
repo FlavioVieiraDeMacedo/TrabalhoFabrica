c9deb201a0eb54cfbb68a5a14d058d58fda0017e
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-26 00:23:59 -0200

    Add FornecedorController

---
+++ LeilaoFabrica/Controllers/FornecedorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeilaoFabrica.Controllers
+{
+    public class FornecedorController : Controller
+    {
+
+        #region GETS
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
