5a2743ad704568b520c9a7ab758cffd3aa03ca1e
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-25 23:57:50 -0200

    Add Produto Controller/ViewModel

---
+++ LeilaoFabrica/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LeilaoFabrica.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        #region FIELDS
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+        [Required]
+        [Display(Name = "Descrição do Produto")]
+        public string Descricao { get; set; }
+        [Required]
+        [Display(Name = "Valor do Produto ")]
+        public decimal PrecoBase { get; set; }
+
+        //TODO: verificar o campo Foto (image?string)
+        //public byte[] Foto { get; set; }
+        #endregion
+
+    }
+}
